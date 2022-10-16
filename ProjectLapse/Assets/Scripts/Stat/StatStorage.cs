648d084f680f7f7a4f919e948d16284016dc93da
Ali Eren Aydın <Aere612@gmail.com>
2022-10-16 21:38:17 +0300

    statstorage optimizasyon+ statsUI temelleri

--- ProjectLapse/Assets/Scripts/Stat/StatStorage.cs
+++ ProjectLapse/Assets/Scripts/Stat/StatStorage.cs
@@ -6,26 +6,8 @@ public class StatStorage : MonoBehaviour
 {
     public List<Stat> statList;
     
-    [SerializeField] private Stat stat1;
-    [SerializeField] private Stat stat2;
-    [SerializeField] private Stat stat3;
-    [SerializeField] private Stat stat4;
-    [SerializeField] private Stat stat5;
-    [SerializeField] private Stat stat6;
-    [SerializeField] private Stat stat7;
-    [SerializeField] private Stat stat8;
     private Stat endingStat;
-    private void Start()
-    {
-        statList.Add(stat1);
-        statList.Add(stat2);
-        statList.Add(stat3);
-        statList.Add(stat4);
-        statList.Add(stat5);
-        statList.Add(stat6);
-        statList.Add(stat7);
-        statList.Add(stat8);
-    }
+
     public bool CheckStats()
     {
         for (int a=0;a!=5;a++)
