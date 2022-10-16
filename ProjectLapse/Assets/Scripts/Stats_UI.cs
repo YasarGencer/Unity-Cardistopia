708ac306936e59238d2c66f0ed052459de182906
Ali Eren Aydın <Aere612@gmail.com>
2022-10-16 20:49:37 +0300

    statstorage fix

---
+++ ProjectLapse/Assets/Scripts/Stats_UI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Stats_UI : MonoBehaviour
+{
+    public Slider s1, s2, s3, s4;
+    public void UpdateStats()
+    {
+        
+    }
+}
