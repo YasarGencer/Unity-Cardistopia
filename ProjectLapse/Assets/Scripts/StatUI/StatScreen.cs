b4bb10b7229de2775e097c7b82e491e053c01311
Yaşar Gençer <gncrysr@gmail.com>
2022-10-23 16:19:46 +0300

    Stat Screen in Dev

---
+++ ProjectLapse/Assets/Scripts/StatUI/StatScreen.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatScreen : MonoBehaviour
+{
+    public GameObject screen;
+    GameObject instantiated;
+    public void Spawn()
+    {
+        instantiated = Instantiate(this.screen);
+        GameObject close = GameObject.Find(instantiated.name + "/Close");
+        close.GetComponent<Button>().onClick.AddListener(Close);
+    }
+    public void Close()
+    {
+        if(instantiated != null)
+            Destroy(instantiated);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Close();
+        }
+    }
+
+}
